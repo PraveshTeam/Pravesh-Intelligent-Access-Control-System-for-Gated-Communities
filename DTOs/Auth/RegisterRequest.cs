c98c6f0bb28fe175b98c821cb74eb4fdd59a84ef
Varad Patil <varadpatil466@gmail.com>
2026-06-30 14:46:03 +0530

    Add password validation

--- DTOs/Auth/RegisterRequest.cs
+++ DTOs/Auth/RegisterRequest.cs
@@ -13,7 +13,8 @@ public class RegisterRequest
     public string Email { get; set; } = string.Empty;
 
     [Required]
-    [MinLength(6)]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
     public string Password { get; set; } = string.Empty;
 
     public string? Phone { get; set; }
