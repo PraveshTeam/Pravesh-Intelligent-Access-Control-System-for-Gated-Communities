cf92778636d4a955b21f461dc8e60d59cb93565b
89285-Salonee <shirsatsalonee510@gmail.com>
2026-06-30 10:58:57 +0530

    feat: add admin service interface

---
+++ Services/Interfaces/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using Pravesh.API.DTOs.Auth;
+
+namespace Pravesh.API.Services.Interfaces;
+
+public interface IAdminService
+{
+    Task<List<UserProfileResponse>> GetAllUsersAsync(int? societyId);
+
+    Task<string> ToggleUserStatusAsync(int id, bool isActive);
+}
