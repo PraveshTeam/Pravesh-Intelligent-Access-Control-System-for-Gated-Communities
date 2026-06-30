400f03aa383a5f1c01307d2313ed7a81f3235f1c
89285-Salonee <shirsatsalonee510@gmail.com>
2026-06-30 18:02:02 +0530

    feat: add society service interface

---
+++ Services/Interfaces/ISocietyService.cs
@@ -0,0 +1,16 @@
+﻿using Pravesh.API.DTOs.Society;
+
+namespace Pravesh.API.Services.Interfaces;
+
+public interface ISocietyService
+{
+    Task<SocietyResponse> CreateSocietyAsync(CreateSocietyRequest req);
+
+    Task<List<FlatResponse>> GetFlatsAsync(int id);
+
+    Task<List<GateResponse>> GetGatesAsync(int id);
+
+    Task<string> AssignSocietyAdminAsync(int id, AssignSocietyAdminRequest req);
+
+    Task<string> AssignUserToSocietyAsync(int id, AssignUserToSocietyRequest req);
+}
